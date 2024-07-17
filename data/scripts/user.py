bd06a0301de1c13443de47a1e01d5accc8dbdc96
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-17 23:45:04 +1000

    scripts updated

--- data/scripts/user.py
+++ data/scripts/user.py
@@ -1,17 +1,13 @@
 from sqlalchemy.orm import Session
 from toaster.database import script
-from toaster.broker.events import Event
 from data import Permission, Staff, StaffRole
 
 
 @script(auto_commit=False)
-def get_user_permission(session: Session, event: Event) -> int:
-    staff = session.get(Staff, {"uuid": event.user.uuid})
+def get_user_permission(session: Session, uuid: int, bpid: int) -> int:
+    staff = session.get(Staff, {"uuid": uuid})
     if (staff is not None) and (StaffRole.TECH == staff.role):
         return 2
 
-    permission = session.get(
-        Permission, {"uuid": event.user.uuid, "bpid": event.peer.bpid}
-    )
-
+    permission = session.get(Permission, {"uuid": uuid, "bpid": bpid})
     return permission.permission.value if permission else 0
