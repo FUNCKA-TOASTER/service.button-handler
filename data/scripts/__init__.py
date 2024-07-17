15bd0f1cd91bdb375dde3c879b525de6a9a8ff55
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-17 19:08:13 +1000

    peer scripts

---
+++ data/scripts/__init__.py
@@ -0,0 +1,13 @@
+from .peer import (
+    get_peer_mark,
+    set_peer_mark,
+)
+from .user import (
+    get_user_permission,
+)
+
+__all__ = (
+    "get_peer_mark",
+    "set_peer_mark",
+    "get_user_permission",
+)
--- data/scripts/peer.py
+++ data/scripts/peer.py
@@ -1,10 +1,21 @@
 from typing import Optional
 from sqlalchemy.orm import Session
+from toaster.broker.events import Event
 from toaster.database import script
-from data import Peer
+from data import Peer, PeerMark
 
 
 @script(auto_commit=False)
 def get_peer_mark(session: Session, bpid: int) -> Optional[str]:
     peer = session.get(Peer, {"id": bpid})
-    return peer.mark if peer else None
+    return peer.mark.value if peer else None
+
+
+@script(auto_commit=False)
+def set_peer_mark(session: Session, mark: str, event: Event) -> Optional[str]:
+    new_mark = Peer(
+        id=event.peer.bpid,
+        name=event.peer.name,
+        mark=PeerMark(mark),
+    )
+    session.add(new_mark)
